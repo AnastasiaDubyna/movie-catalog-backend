b09f92f4e08237c43ce9b93d7bfd211c60a38772
Anastasiia Dubyna <anastasiia.dubyna@dataart.com>
2023-01-20 13:40:48 +0100

    schemas added, connection established

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const ratingRouter = require('./router/ratingRouter');
 const mongoose = require('mongoose');
+const Media = require('./models/Media');
+const Review = require('./models/Review');
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -14,4 +16,15 @@ app.use('/', jsonParser, ratingRouter);
 app.listen(5000, () => console.log(`Listening on port 5000`));
 
 mongoose.connect(process.env.MONGO_CONNECTION_LINK)
-    .then(_ => app.listen(5000, () => console.log("Server is running")))
+    .then(_ => app.listen(8080, () => {
+        console.log("Server is running");
+        Media.collection.drop();
+        Review.collection.drop();
+        const movie = new Media({tmdb_id: "12345"});
+        const comment = new Review({content: "test comment", username: "KittyKat", title: "No so bad"});
+        movie.reviews.push(comment);
+        comment.save()
+        movie.save()
+            .then((_) => console.log("saved"))
+        console.log(movie.reviews)
+    }));
